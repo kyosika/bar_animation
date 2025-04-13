5f1ca66a26394fc1768805b680901c3cfd233f58
Kyosika Dev <ntaflove@gmail.com>
2025-04-13 15:15:22 -0700

    Update random_interval.cpp

--- random_interval.cpp
+++ random_interval.cpp
@@ -7,8 +7,8 @@ int main()
         srand(time(NULL));
             int counter = std::rand() % 125 + 1;
         for(int i = 0;i < counter; i++){
-auto seconds = std::rand() % 3 + 1;
-int between = std::rand() % 10 + 1;
+auto seconds = std::rand() % 3 + 1; //Possibly for backup but not completely needed
+int between = std::rand() % 10 + 1; //Also backup
 int milli = std::rand() % 400 + 1 + 100;
 auto sleep_duration = std::chrono::milliseconds(milli);
 
