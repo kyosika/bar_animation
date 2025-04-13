b2b11e79f9c7d78891845f8110a038f84659f60c
Kyosika Dev <ntaflove@gmail.com>
2025-04-13 15:40:54 -0700

    Create percent.cpp

---
+++ percent.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <cstdlib>
+int main(){
+    srand(time(NULL));
+      for(int i = 1; i <= 100; i++){
+    int sec = std::rand() % 5;
+    if(sec == 0){
+        
+        sec = 1;
+    }
+    auto percent_time = std::chrono::seconds(sec);
+  
+        std::cout << i << "%" << std::endl << std::flush;
+             
+        std::this_thread::sleep_for(percent_time);
+        
+        
+         system("clear");
+  
+        
+    }
+    return 0;
+}
